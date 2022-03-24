cbf63c85cc126d2b743649b7f64b842abc6a8ba2
SyahinSyah <syahinsyah7@gmail.com>
2022-03-24 14:44:02 +0800

    first commit

---
+++ LikomExercise/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LikomExercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
